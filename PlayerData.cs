79fc94718ae249663f7deaf432ee6a03e71f7efd
/Dom <32508991+domroutley@users.noreply.github.com>
2026-03-09 22:25:01 +0000

    update for player names

--- PlayerData.cs
+++ PlayerData.cs
@@ -3,6 +3,9 @@ using Newtonsoft.Json.Linq;
 using Steamworks;
 using System;
 using UnityEngine;
+using static DvMod.RemoteDispatch.World;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace DvMod.RemoteDispatch
 {
@@ -57,26 +60,41 @@ namespace DvMod.RemoteDispatch
                 new JProperty("color", "aqua"),
                 new JProperty("position", previousPosition.ToLatLon().ToJson()),
                 new JProperty("rotation", Math.Round(previousRotation, 2)),
-                new JProperty("name", GetLocalSteamName())
+                new JProperty("name", $"{GetLocalSteamName()} - host")
             );
 
-            var otherPlayers = MultiplayerHook.GetServerPlayers();
+            IEnumerable<object> otherPlayers;
+            try
+            {
+                otherPlayers = MultiplayerHook.GetServerPlayers();
+            }
+            catch (Exception e)
+            {
+                Main.DebugLog(() => $"Error getting server players: {e.Message}");
+                return res;
+            }
+            Main.DebugLog(() => $"Player count: {MultiplayerHook.GetPlayerCount()}");
+
             if (otherPlayers != null)
             {
+                Main.DebugLog(() => $"Other players found!");
                 foreach (var player in otherPlayers)
                 {
                     var type = player.GetType();
+                     Main.DebugLog(() => $"Player properties: {string.Join(", ", type.GetProperties().Select(p => p.Name))}");
                     // https://github.com/AMacro/dv-multiplayer/blob/beta/Multiplayer/Networking/Data/ServerPlayer.cs
                     var absPos = (Vector3)type.GetProperty("AbsoluteWorldPosition").GetValue(player);
+                    var position = new World.Position(absPos - WorldMover.currentMove);
                     var rotY = (float)type.GetProperty("WorldRotationY").GetValue(player);
                     var name = (string)type.GetProperty("Username").GetValue(player);
                     var guid = (Guid)type.GetProperty("Guid").GetValue(player);
                     var playerId = (byte)type.GetProperty("PlayerId").GetValue(player);
+
                     res[guid.ToString()] = new JObject(
                         new JProperty("color", "orange"),
-                        new JProperty("position", ((World.Position)player.GetType().GetProperty("Position").GetValue(player)).ToLatLon().ToJson()),
-                        new JProperty("rotation", Math.Round((float)player.GetType().GetProperty("Rotation").GetValue(player), 2)),
-                        new JProperty("name", player.GetType().GetProperty("Name").GetValue(player)?.ToString() ?? string.Empty)
+                        new JProperty("position", position.ToLatLon().ToJson()),
+                        new JProperty("rotation", Math.Round(rotY, 2)),
+                        new JProperty("name", name ?? string.Empty)
                     );
                 }
             }
